f23dd300c9058b3e33ddac4ca4515d9990c1cb0e
S0mbr4xc <s3._xc@hotmail.com>
2024-07-16 09:08:20 -0500

    a

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { cliente } from '../../domain/cliente';
+import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
+  ci: any
+  cliente: cliente = new cliente
+  deudas: any
 
-  
+  constructor(private clienteService: ClienteService ){}
+
+  listar(){
+    this.clienteService.getCliente(this.ci).subscribe(data => {
+      this.cliente = data;
+    });
+
+    this.clienteService.getDeuda().subscribe(data => {
+      this.deudas = data;
+      console.log(this.deudas)
+    });
+  }
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class ClienteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCliente(ci: String){
+    let url = "http://localhost:8080/pruebaJEE/rs/cliente/"+ci
+    return this.http.get<any>(url)
+  }
+
+  getDeuda(){
+    let url = "http://localhost:8080/pruebaJEE/rs/deuda"
+    return this.http.get<any>(url)
+  }
 }
